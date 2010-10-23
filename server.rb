b211a605011aa84a130b9ce74b9e31ca0153c193
Mathias Meyer <meyer@paperplanes.de>
2010-10-23 17:16:47 +0200

    Wrap a runnable class in the processor.

--- server.rb
+++ server.rb
@@ -2,34 +2,54 @@ require 'rubygems'
 require 'eventmachine'
 require 'json'
 
-class SingleProcess
-  def initialize(port)
-    @port = port
+class Runner
+  def initialize(options = {})
   end
 
   def run
+    puts "Running!"
+  end
+end
+
+class SingleProcess
+  def initialize(port, runnable, options = {})
+    @port = port
+    @runnable = runnable
+    @options = options
+  end
+
+  def fork
     EM.fork_reactor do
       $0 = "<child>"
-      @connection = EM.connect('127.0.0.1', @port) do |c|
-        class << c
-          include EM::P::ObjectProtocol
-        end
-      end
+      setup
+      run
+    end
+  end
 
-      EM.add_periodic_timer(2) do
-        if @connection.error?
-          @connection.reconnect('127.0.0.1', @port)
-        end
-        @connection.send_object([:heartbeat, $$])
+  def setup
+    @connection = EM.connect('127.0.0.1', @port) do |c|
+      class << c
+        include EM::P::ObjectProtocol
       end
     end
+
+    EM.add_periodic_timer(2) do
+      if @connection.error?
+        @connection.reconnect('127.0.0.1', @port)
+      end
+      @connection.send_object([:heartbeat, $$])
+    end
+  end
+
+  def run
+    @runnable.new(@options).run
   end
 end
 
 class ProcessMonitor
   class Server < EM::Connection
-    attr_accessor :owner
     include EM::P::ObjectProtocol
+    attr_accessor :owner
 
     def receive_object(object)
       owner.receive_message(object)
@@ -42,7 +62,7 @@ class ProcessMonitor
 
   def run
     start_server
-    start_process
+    start_processes
   end
 
   def receive_message(msg)
@@ -56,8 +76,8 @@ class ProcessMonitor
     @port = Socket.unpack_sockaddr_in(EM.get_sockname(@server)).first
   end
 
-  def start_process
-    SingleProcess.new(@port).run
+  def start_processes
+    SingleProcess.new(@port, Runner, {}).run
   end
 end
 
